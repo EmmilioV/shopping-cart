a41cd1c6a12cafe571dbd8490765f57886ea545f
EmmilioV <emmiliov.p@gmail.com>
2022-01-03 17:16:29 -0500

    primera parte del demo

---
+++ src/actions/phones.js
@@ -0,0 +1,10 @@
+import phones from "../reducers/phones"
+
+export const RECEIVE_PHONES = 'RECEIVE_PHONES'
+
+export function receivePhones(phones){
+    return {
+        type: RECEIVE_PHONES,
+        phones
+    }
+}
